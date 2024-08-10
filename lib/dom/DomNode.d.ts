c0eea1ffd303e949c26eb84598eb5e7a57a1b581
yjgaia <yjdev88@gmail.com>
2024-08-10 12:53:44 +0900

    dev

--- lib/dom/DomNode.d.ts
+++ lib/dom/DomNode.d.ts
@@ -1,10 +1,17 @@
+import { EventContainer } from "@common-module/ts";
 import DomSelector from "./DomSelector.js";
 export type DomChild = DomNode | string;
-export default class DomNode<HE extends HTMLElement = HTMLElement> {
-    private htmlElement;
+export default class DomNode<HE extends HTMLElement = HTMLElement, ET extends Record<string, (...args: any[]) => any> = {}> extends EventContainer<ET & {
+    visible: () => void;
+}> {
+    private parent;
+    private children;
+    protected htmlElement: HE;
     constructor(htmlElement?: HE | DomSelector, ...children: DomChild[]);
     private appendText;
     append(...children: DomChild[]): void;
+    private isVisible;
+    private notifyVisibility;
     appendTo(parent: DomNode, index?: number): this;
     delete(): void;
     empty(): this;
--- src/dom/DomNode.ts
+++ src/dom/DomNode.ts
@@ -1,12 +1,20 @@
+import { EventContainer } from "@common-module/ts";
 import DomSelector from "./DomSelector.js";
 import DomUtil from "./DomUtil.js";
 
 export type DomChild = DomNode | string;
 
-export default class DomNode<HE extends HTMLElement = HTMLElement> {
-  private htmlElement: HE;
+export default class DomNode<
+  HE extends HTMLElement = HTMLElement,
+  ET extends Record<string, (...args: any[]) => any> = {},
+> extends EventContainer<ET & { visible: () => void }> {
+  private parent: DomNode | undefined;
+  private children: DomNode[] = [];
+
+  protected htmlElement: HE;
 
   constructor(htmlElement?: HE | DomSelector, ...children: DomChild[]) {
+    super();
     this.htmlElement = htmlElement instanceof HTMLElement
       ? htmlElement
       : DomUtil.createHtmlElement<HE>(htmlElement ?? "");
@@ -39,6 +47,25 @@ export default class DomNode<HE extends HTMLElement = HTMLElement> {
     }
   }
 
+  private isVisible(): boolean {
+    let currentNode: DomNode | undefined = this;
+    while (currentNode !== undefined) {
+      if (currentNode.htmlElement === document.body) {
+        return true;
+      }
+      currentNode = currentNode.parent;
+    }
+    return false;
+  }
+
+  private notifyVisibility() {
+    this.emit(
+      "visible",
+      ...([] as Parameters<(ET & { visible: () => void })["visible"]>),
+    );
+    this.children.forEach((child) => child.notifyVisibility());
+  }
+
   public appendTo(parent: DomNode, index?: number): this {
     if (index === undefined || index >= parent.htmlElement.childNodes.length) {
       parent.htmlElement.appendChild(this.htmlElement);
@@ -46,6 +73,8 @@ export default class DomNode<HE extends HTMLElement = HTMLElement> {
       const referenceNode = parent.htmlElement.childNodes[index];
       parent.htmlElement.insertBefore(this.htmlElement, referenceNode);
     }
+    this.parent = parent;
+    if (this.isVisible()) this.notifyVisibility();
     return this;
   }
 
