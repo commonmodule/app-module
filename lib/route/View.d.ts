c325322301c10d49830019d6b156ff1829558b26
yjgaia <yjdev88@gmail.com>
2024-09-26 18:26:49 +0900

    Refactor View class to add event handling and cleanup

--- lib/route/View.d.ts
+++ lib/route/View.d.ts
@@ -1,10 +1,17 @@
+import { EventContainer } from "@common-module/ts";
 import DomNode from "../dom/DomNode.js";
 export interface ViewParams {
     [name: string]: string | undefined;
 }
 export default abstract class View<CT extends DomNode = DomNode> {
+    private eventListeners;
     protected container: CT;
     changeParams(params: ViewParams): void;
+    protected addEvent<T extends EventContainer<ET>, ET extends Record<string, (...args: any[]) => any>, K extends keyof ET>(target: T, eventName: K, listener: (ET & {
+        visible: () => void;
+        remove: () => void;
+    })[K]): this;
+    private removeAllEvents;
     close(): void;
 }
 //# sourceMappingURL=View.d.ts.map
--- src/route/View.ts
+++ src/route/View.ts
@@ -1,3 +1,4 @@
+import { EventContainer } from "@common-module/ts";
 import DomNode from "../dom/DomNode.js";
 
 export interface ViewParams {
@@ -5,10 +6,44 @@ export interface ViewParams {
 }
 
 export default abstract class View<CT extends DomNode = DomNode> {
+  private eventListeners: Map<EventContainer<any>, Map<string, EventListener>> =
+    new Map();
+
   protected container!: CT;
 
   public changeParams(params: ViewParams): void {}
+
+  protected addEvent<
+    T extends EventContainer<ET>,
+    ET extends Record<string, (...args: any[]) => any>,
+    K extends keyof ET,
+  >(
+    target: T,
+    eventName: K,
+    listener: (ET & { visible: () => void; remove: () => void })[K],
+  ): this {
+    if (!this.eventListeners.has(target)) {
+      this.eventListeners.set(target, new Map());
+    }
+
+    const targetListeners = this.eventListeners.get(target)!;
+    targetListeners.set(eventName as string, listener);
+    target.on(eventName, listener);
+
+    return this;
+  }
+
+  private removeAllEvents(): void {
+    for (const [target, listeners] of this.eventListeners) {
+      for (const [eventName, listener] of listeners) {
+        target.off(eventName, listener);
+      }
+    }
+    this.eventListeners.clear();
+  }
+
   public close(): void {
-    this.container?.remove();
+    this.removeAllEvents();
+    this.container.remove();
   }
 }
